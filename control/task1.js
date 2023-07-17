960a44086195ba26a9fa78fb94208b970b109af9
Ramesh-Kalimuthu <krameshkpk20@gmail.com>
2023-07-17 15:26:08 +0530

    control statement task1 added

---
+++ control/task1.js
@@ -0,0 +1,15 @@
+let x = 3
+let y = -7
+let z = 2
+if (x > 0 && y > 0 && z > 0) {
+    alert("the value is +")
+}
+else if (x < 0 && y < 0 && z < 0) {
+    alert("the value is -")
+}
+else if (x > 0 && y < 0 && z > 0) {
+    alert("the value is ")
+}
+else {
+    alert("enter correct value")
+}
