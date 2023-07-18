9508c0297c55458dc9806d54478c95b4ee89de1d
Ramesh-Kalimuthu <krameshkpk20@gmail.com>
2023-07-18 11:18:15 +0530

    control largest num task 3

---
+++ control/task3.js
@@ -0,0 +1,11 @@
+let num1 = prompt("num1")
+let num2 = prompt("num2")
+if (num1 > num2) {
+    alert("num1 is largest")
+}
+else if (num1 < num2) {
+    alert("num2 is largest")
+}
+else {
+    alert("num1==num2")
+}
