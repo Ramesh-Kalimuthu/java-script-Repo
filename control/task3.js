c39af30437b5de8d52912d24d38d4d4c1c0dc3de
Ramesh-Kalimuthu <krameshkpk20@gmail.com>
2023-07-18 15:49:07 +0530

    new

--- control/task3.js
+++ control/task3.js
@@ -1,10 +1,10 @@
 let num1 = prompt("num1")
 let num2 = prompt("num2")
 if (num1 > num2) {
-    alert("num1 is largest")
+    alert(num1+"is largest")
 }
 else if (num1 < num2) {
-    alert("num2 is largest")
+    alert(num2+" is largest")
 }
 else {
     alert("num1==num2")
