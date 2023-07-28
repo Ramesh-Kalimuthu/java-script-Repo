17beeea90c689dfc81ee557829edd98555605df6
Ramesh-Kalimuthu <krameshkpk20@gmail.com>
2023-07-28 16:10:38 +0530

    string task

---
+++ string/task1.js
@@ -0,0 +1,7 @@
+let name = "Ramesh Kalimuthu"
+let arr = []
+let a = name.slice(0, 6)
+let b = name.slice(7,)
+arr[0] = a
+arr[1] = b
+console.log(arr)
---
+++ string/task2.js
@@ -0,0 +1,7 @@
+let mail = prompt("Enter email: ")
+let hide = mail.slice(0, 4) + "***" + mail.slice(10)
+console.log(hide)
+
+// let text = "kramesh123@gmail.com";
+// let result = text.replace("esh123", "***");
+// console.log(result)
